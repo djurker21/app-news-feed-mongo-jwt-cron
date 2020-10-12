2206ab23f12e71444983b88449490dc5385fb514
Yurii Vydai <djurker21@gmail.com>
2020-10-12 09:15:07 +0300

    create, delete

--- news/news.controller.js
+++ news/news.controller.js
@@ -4,11 +4,25 @@ const newsService = require('./news.service');
 
 // routes
 router.get('/', getAll);
+router.post('/', create);
+router.delete('/:id', _delete);
 
 module.exports = router;
 
 function getAll(req, res, next) {
     newsService.getAll()
         .then(news => res.json(news))
+        .catch(err => next(err));
+}
+
+function create(req, res, next) {
+    newsService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    newsService.delete(req.params.id)
+        .then(() => res.json({}))
         .catch(err => next(err));
 }
--- news/news.service.js
+++ news/news.service.js
@@ -5,6 +5,17 @@ async function getAll() {
     return await News.find();
 }
 
+async function create(newsParam) {
+    const news = new News(newsParam);
+    await news.save();
+}
+
+async function _delete(id) {
+    await News.findByIdAndRemove(id);
+}
+
 module.exports = {
-    getAll
+    getAll,
+    create,
+    delete: _delete
 };
